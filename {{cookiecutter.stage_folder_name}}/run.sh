b45cfbc1b3d45d0ca450301829363ef93e96d68f
horothesun <horothesun@gmail.com>
2021-04-11 17:25:37 +0100

    Required environment variables error handling

--- {{cookiecutter.stage_folder_name}}/run.sh
+++ {{cookiecutter.stage_folder_name}}/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ -z "${{ cookiecutter.stage_env_var_prefix }}INPUT_FILE" ] && echo "Error: {{ cookiecutter.stage_env_var_prefix }}INPUT_FILE must be defined" && exit 10
+[ -z "${{ cookiecutter.stage_env_var_prefix }}OUTPUT_DIR" ] && echo "Error: {{ cookiecutter.stage_env_var_prefix }}OUTPUT_DIR must be defined" && exit 20
+
 echo "{{ cookiecutter.stage_env_var_prefix }}INPUT_FILE: ${{ cookiecutter.stage_env_var_prefix }}INPUT_FILE"
 echo "{{ cookiecutter.stage_env_var_prefix }}OUTPUT_DIR: ${{ cookiecutter.stage_env_var_prefix }}OUTPUT_DIR"
 
